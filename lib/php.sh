67436ccc9481e8428885c5bc7ef331112e696e5c
Sisir Adhikari <sisir@codeware.io>
2026-06-30 19:50:35 +0600

    Fixed php socket path

--- lib/php.sh
+++ lib/php.sh
@@ -6,14 +6,14 @@
 
 detect_php() {
 
-    info "Detecting PHP..."
+	info "Detecting PHP..."
 
-    PHP_VERSION="$(php -r 'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;')"
-    PHP_SOCKET="unix//run/php/php${PHP_VERSION}-fpm.sock"
+	PHP_VERSION="$(php -r 'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;')"
+	PHP_SOCKET="/run/php/php${PHP_VERSION}-fpm.sock"
 
-    [[ -S "$PHP_SOCKET" ]] || die "PHP-FPM socket not found: $PHP_SOCKET"
+	[[ -S "$PHP_SOCKET" ]] || die "PHP-FPM socket not found: $PHP_SOCKET"
 
-    PHP_BIN="$(command -v php)"
-    success "Detected PHP $PHP_VERSION"
+	PHP_BIN="$(command -v php)"
+	success "Detected PHP $PHP_VERSION"
 
 }
